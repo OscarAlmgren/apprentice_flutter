d3906cc4537a6e4826cd21f62607f5ec08f6c5be
Oscar Almgren <oscar.almgren@gmail.com>
2022-02-11 14:57:17 +0100

    refactor Fooderlich to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-import 'fooderlich.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
   runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override // alla stateless widgets måste override:a build()
+  Widget build(BuildContext context) {
+    final lightTheme = FooderlichTheme.light();
+    // final darkTheme = FooderlichTheme.dark();
+
+    return MaterialApp(
+      theme: lightTheme,
+      title: 'Fooderlich OA',
+      home: const Home(),
+    );
+  }
 }
