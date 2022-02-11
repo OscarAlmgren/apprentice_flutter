886feb521396d1e2a8a2ca2e4b920ca9eb22f23c
Oscar Almgren <oscar.almgren@gmail.com>
2022-02-11 13:43:51 +0100

    Add containers, stack, wraps and chips

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,3 +1,4 @@
+import 'package:apprentice_flutter/fooderlich_theme.dart';
 import 'package:flutter/material.dart';
 
 class Card3 extends StatelessWidget {
@@ -21,10 +22,77 @@ class Card3 extends StatelessWidget {
           ),
         ),
         child: Stack(
-          children: const [
-            // TODO 5: add dark overlay BoxDecoration
-            // TODO 6: Add Container, Column, Icon and Text
-            // TODO 7: Add Center widget with Chip widget children
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.headline2,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Wrap(
+                alignment: WrapAlignment.start,
+                spacing: 12.0,
+                runSpacing: 12.0,
+                children: [
+                  Chip(
+                    label: Text(
+                      'Healthy',
+                      style: FooderlichTheme.darkTextTheme.bodyText1,
+                    ),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text(
+                      'Vegan',
+                      style: FooderlichTheme.darkTextTheme.bodyText1,
+                    ),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text(
+                      'Carrots',
+                      style: FooderlichTheme.darkTextTheme.bodyText1,
+                    ),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
