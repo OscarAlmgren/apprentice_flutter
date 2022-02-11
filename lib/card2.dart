6309ba3a280d446013f961fa3e63fe885dd2d5f1
Oscar Almgren <oscar.almgren@gmail.com>
2022-02-11 11:39:29 +0100

    Add labels and rotation to text fields for design

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,6 +1,8 @@
 import 'package:apprentice_flutter/author_card.dart';
 import 'package:flutter/material.dart';
 
+import 'fooderlich_theme.dart';
+
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
 
@@ -22,12 +24,37 @@ class Card2 extends StatelessWidget {
           ),
         ),
         child: Column(
-          children: const [
-            AuthorCard(
+          children: [
+            const AuthorCard(
               authorName: 'Oscar Almgren',
               title: 'Smoothie lover 🧉',
               imageProvider: AssetImage('assets/author_katz.jpeg'),
-            )
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
