b699e829bef21ec493084bbbdc233766a291dcbc
Oscar Almgren <oscar.almgren@gmail.com>
2022-02-11 13:16:51 +0100

    add container with background image

--- lib/card3.dart
+++ lib/card3.dart
@@ -5,8 +5,29 @@ class Card3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        child: Stack(
+          children: const [
+            // TODO 5: add dark overlay BoxDecoration
+            // TODO 6: Add Container, Column, Icon and Text
+            // TODO 7: Add Center widget with Chip widget children
+          ],
+        ),
+      ),
     );
   }
 }
