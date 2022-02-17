d025e6aa17ad1a0333590a976e2429b56fb3d64f
Oscar Almgren <oscar.almgren@gmail.com>
2022-02-17 07:57:20 +0100

    ScrollController, listener and MaxExtent for gridvid

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
   int _selectedNavigationIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    ExploreScreen(),
+    const ExploreScreen(),
     RecipesScreen(),
     Container(
       color: Colors.cyan,
--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -4,10 +4,41 @@ import 'package:flutter/material.dart';
 import '../components/components.dart';
 import '../models/models.dart';
 
-class ExploreScreen extends StatelessWidget {
-  final mockService = MockFooderlichService();
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
 
-  ExploreScreen({Key? key}) : super(key: key);
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _controller;
+
+  void _scrollListerner() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('At the bottom!');
+    }
+
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('At the top!');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListerner);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListerner);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +47,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _controller,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
