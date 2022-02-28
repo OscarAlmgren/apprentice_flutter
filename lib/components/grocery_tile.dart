a4f413c55ef0679f88e0c770bcb08bd182e6b198
Oscar Almgren <oscar.almgren@gmail.com>
2022-02-28 11:01:49 +0100

    GroceryTile layout widgets and buildCheckbox() added

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -23,8 +23,45 @@ class GroceryTile extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // todo 22
-          // todo 23
+          Row(
+            children: [
+              Container(
+                width: 5.0,
+                color: item.color,
+              ),
+              const SizedBox(
+                width: 16.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
+                      decoration: _textDecoration,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance(),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                    decoration: _textDecoration, fontSize: 21.0),
+              ),
+              buildCheckbox(),
+            ],
+          ),
         ],
       ),
     );
@@ -57,7 +94,7 @@ class GroceryTile extends StatelessWidget {
   }
 
   Widget buildDate() {
-    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateFormatter = DateFormat('MMM dd hh:mm a');
     final dateString = dateFormatter.format(item.date);
     return Text(
       dateString,
