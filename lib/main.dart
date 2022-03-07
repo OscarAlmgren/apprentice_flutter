c0dba6c9764367984ad881f1ad804bf6b23d32f4
Oscar Almgren <oscar.almgren@gmail.com>
2022-03-07 13:26:41 +0100

    moved to stateful widget and theme settings option

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,60 @@
 import 'package:apprentice_flutter/models/grocery_manager.dart';
+import 'package:apprentice_flutter/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'models/models.dart';
 
 import 'fooderlich_theme.dart';
-import 'home.dart';
+import 'screens/home.dart';
 
 void main() {
   runApp(const Fooderlich());
 }
 
-class Fooderlich extends StatelessWidget {
+class Fooderlich extends StatefulWidget {
   const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  State<Fooderlich> createState() => _FooderlichState();
+}
+
+class _FooderlichState extends State<Fooderlich> {
+  final _groceryManager = GroceryManager();
+  final _profileManager = ProfileManager();
 
   @override // alla stateless widgets måste override:a build()
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.light();
-    // final theme = FooderlichTheme.dark();
-
-    return MaterialApp(
-      theme: theme,
-      title: 'Fooderlich OA',
-      home: MultiProvider(providers: [
-        ChangeNotifierProvider(create: (context) => TabManager()),
-        ChangeNotifierProvider(create: (context) => GroceryManager()),
-      ], child: const Home()),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => _groceryManager),
+        ChangeNotifierProvider(create: (context) => _profileManager),
+        // TODO: Add AppStateManager ChangeNotifierProvider
+      ],
+      child: Consumer<ProfileManager>(
+        builder: (context, profileManager, child) {
+          ThemeData theme;
+          if (profileManager.darkMode) {
+            theme = FooderlichTheme.dark();
+          } else {
+            theme = FooderlichTheme.light();
+          }
+          return MaterialApp(
+            theme: theme,
+            title: 'Fooderlich',
+            // replace with router widget
+            home: const SplashScreen(),
+          );
+        },
+      ),
     );
+
+    // return MaterialApp(
+    //   theme: theme,
+    //   title: 'Fooderlich OA',
+    //   home: MultiProvider(providers: [
+    //     ChangeNotifierProvider(create: (context) => _profileManager),
+    //     ChangeNotifierProvider(create: (context) => _groceryManager),
+    //   ], child: const Home()),
+    // );
   }
 }
