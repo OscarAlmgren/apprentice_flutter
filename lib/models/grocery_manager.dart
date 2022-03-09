550b30012e3ae4eebaecd255ddda21b394af0a4b
Oscar Almgren <oscar.almgren@gmail.com>
2022-03-09 08:18:38 +0100

    GroceryManager updated with new get(s) and additional _private fields

--- lib/models/grocery_manager.dart
+++ lib/models/grocery_manager.dart
@@ -3,21 +3,41 @@ import 'package:flutter/material.dart';
 
 class GroceryManager extends ChangeNotifier {
   final _groceryItems = <GroceryItem>[];
+  int _seletedIndex = -1;
+  bool _createNewItem = false;
 
   List<GroceryItem> get groceryItems => List.unmodifiable(_groceryItems);
+  int get selectedIndex => _seletedIndex;
+  GroceryItem? get selectedGroceryItem =>
+      _seletedIndex != -1 ? _groceryItems[_seletedIndex] : null;
+  bool get isCreatingNewItem => _createNewItem;
+
+  void createNewItem() {
+    _createNewItem = true;
+    notifyListeners();
+  }
 
   void deleteItem(int index) {
     _groceryItems.removeAt(index);
     notifyListeners();
   }
 
+  void groceryItemTapped(int index) {
+    _seletedIndex = index;
+    _createNewItem = false;
+    notifyListeners();
+  }
+
   void addItem(GroceryItem item) {
     _groceryItems.add(item);
+    _createNewItem = false;
     notifyListeners();
   }
 
   void updateItem(GroceryItem item, int index) {
     _groceryItems[index] = item;
+    _seletedIndex = -1;
+    _createNewItem = false;
     notifyListeners();
   }
 
